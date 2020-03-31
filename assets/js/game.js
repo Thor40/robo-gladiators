3d9f89210a014d1e8be9480737c59390d6c29781
Thor40 <nicholas.kosik@gmail.com>
2020-03-30 19:28:06 -0500

    NPC/player attack, alert health

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,15 +16,28 @@ var fight = function() {
     // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
 enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
-console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
- );
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+    // check emeny's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " had died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " healthy left.");
+    }
     // Subtract the value of 'enemyAttack' from the value of 'playerhealth and use that result to update the value in the 'playerHealth' variable.
 playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so we know that it worked. 
-console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+    // check player health
+    if (playerHealth <=0) {
+        window.alert(playerName + " had died!");
+    }
+    else {
+        window.alert(playerName + " still had " + playerHealth + " health left.");
+    }
+
 };
 
 // execute function
