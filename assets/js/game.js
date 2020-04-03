2a0d22da889550c1b77e5ded6d5ddad24bf47564
Thor40 <nicholas.kosik@gmail.com>
2020-04-03 10:44:09 -0500

    shop() interget input

--- assets/js/game.js
+++ assets/js/game.js
@@ -133,19 +133,17 @@ var endGame = function() {
 
 var shop = function() {
     // Ask player what they'd like to do
-    var shopOptionPrompt = window.prompt ("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    var shopOptionPrompt = window.prompt ("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
     // Use switch to carry out action
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             // Do nothing, function ends
             break;
